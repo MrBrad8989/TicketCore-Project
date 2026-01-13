6b77ea413aeb374f0ef64e1fa95d6da86ea57523
MrBrad8989 <remussabou02@gmail.com>
2026-01-13 09:45:13 +0100

    Initial Commit

---
+++ src/main/java/es/iesjuanbosco/ticketcoreproject/TicketCoreProjectApplication.java
@@ -0,0 +1,13 @@
+package es.iesjuanbosco.ticketcoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCoreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketCoreProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/iesjuanbosco/ticketcoreproject/TicketCoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesjuanbosco.ticketcoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCoreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
